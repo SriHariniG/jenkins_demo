696fa983948a1f31b7e1c0809f30124ce8708a66
Sri Harini <srihariniganesanr@gmail.com>
2025-07-23 15:22:27 +0530

    first commit

---
+++ icl/src/main/java/com/example/icl/IclApplication.java
@@ -0,0 +1,13 @@
+package com.example.icl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IclApplication.class, args);
+	}
+
+}
---
+++ icl/src/test/java/com/example/icl/IclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
